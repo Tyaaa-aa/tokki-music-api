ee55c582f0761659f94a806b0760a0378977c25b
satya ratnam <sirtya@gmail.com>
2022-05-08 05:03:26 +0800

    Create tokki-music-api

---
+++ index.js
@@ -0,0 +1,63 @@
+const stream = require('youtube-audio-stream')
+async function handleView(req, res) {
+    try {
+        for await (const chunk of stream(`${req.query.videoid}`)) {
+            res.write(chunk)
+        }
+        res.end()
+    } catch (err) {
+        console.error(err)
+        if (!res.headersSent) {
+            res.writeHead(500)
+            res.end('internal system error')
+        }
+    }
+}
+
+const express = require('express')
+// import * as express from 'express';
+
+var cors = require('cors')
+// import * as cors from 'cors';
+
+const app = express()
+
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
+
+app.use(cors())
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true)
+
+    // Pass to next layer of middleware
+    next()
+})
+
+app.get('/api', async function (req, res) {
+    // const url = req.query.search
+    // // let info = await getData(url)
+    // const videos = await yt.search(url);
+    // res.send(videos)
+    // console.log(videos)
+
+    handleView(req, res)
+})
+
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+    // console.log(`Example app listening on port ${app}`)
+})
