7de73d6f0e657c5b5ecbe36d76527711d6d1b80f
satya ratnam <sirtya@gmail.com>
2022-05-12 03:27:36 +0800

    make it better

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.get('/api2', async function (req, res) {
     let info = await ytdl(url, {
         filter: 'audioonly'
     })
-
+    
     info.pipe(res)
 })
 
@@ -81,14 +81,14 @@ app.listen(PORT, () => {
 
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
+// Listen on a specific port via the PORT environment variable
+var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
-
-
-app.get('/proxy', cors_proxy.createServer({
+cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(PORT, host, function () {
-    console.log('Running CORS Anywhere on ' + host + ':' + PORT);
-}))
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
