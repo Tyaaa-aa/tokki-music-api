76207f093f5c71171c89fe793e403ecd3a863913
satya ratnam <sirtya@gmail.com>
2022-05-12 03:25:39 +0800

    make it better

--- index.js
+++ index.js
@@ -89,6 +89,6 @@ app.get('/proxy', cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function () {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+}).listen(PORT, host, function () {
+    console.log('Running CORS Anywhere on ' + host + ':' + PORT);
 }))
